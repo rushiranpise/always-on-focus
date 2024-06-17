655297c7acd7e7ca48a5358f865e91d24b158750
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2024-06-16 20:50:48 -0700

    Update alwaysonfocus.user.js

--- alwaysonfocus.user.js
+++ alwaysonfocus.user.js
@@ -2,21 +2,21 @@
 // @name          Always on focus
 // @namespace     https://github.com/daijro/always-on-focus
 // @author        daijro
-// @version       1.5.2
+// @version       1.5.3
 // @description   Prevents websites from knowing that you switched tabs or unfocused the window
 // @include       *
 // @updateURL     https://github.com/daijro/always-on-focus/raw/main/alwaysonfocus.user.js
 // @downloadURL   https://github.com/daijro/always-on-focus/raw/main/alwaysonfocus.user.js
 // @run-at        document-start
 // ==/UserScript==
-
-
+ 
+ 
 unsafeWindow.onblur = null;
 unsafeWindow.blurred = false;
-
+ 
 unsafeWindow.document.hasFocus = () => true;
 unsafeWindow.window.onFocus = () => true;
-
+ 
 // kill dom property names
 [
     "hidden",
@@ -26,13 +26,13 @@ unsafeWindow.window.onFocus = () => true;
 ].forEach(prop_name => {
     Object.defineProperty(document, prop_name, {value: false});
 })
-
+ 
 Object.defineProperty(document, "visibilityState", {get: () => "visible"});
 Object.defineProperty(document, "webkitVisibilityState", {get: () => "visible"});
-
+ 
 unsafeWindow.document.onvisibilitychange = undefined;
-
-
+ 
+ 
 // element constructors to allow blur events on
 const blurWhitelist = [
     HTMLInputElement,
@@ -40,7 +40,7 @@ const blurWhitelist = [
     HTMLSpanElement,
     HTMLParagraphElement,
 ]
-
+ 
 // element constructors to block mouseleave and mouseout events on
 const hoverBlacklist = [
     HTMLIFrameElement,
@@ -50,12 +50,12 @@ const hoverBlacklist = [
     HTMLFrameSetElement, // obsolete but included for completeness
     HTMLFrameElement // obsolete but included for completeness
 ];
-
+ 
 var event_handler = (event) => {
     // if the event is blur, and the target is an whitelisted type, allow it
     if (event.type === 'blur' &&
         ((blurWhitelist.some(type => event.target instanceof type) ||
-            event.target.classList.contains('ql-editor')))) { // quill js fix
+            event.target.classList?.contains('ql-editor')))) { // quill js fix
         return;
     }
     // if the event is mouseleave or mouseout & pointerleave or pointerout, and the target is an blacklisted type, block it
@@ -67,7 +67,7 @@ var event_handler = (event) => {
     event.stopPropagation();
     event.stopImmediatePropagation();
 }
-
+ 
 // kill event listeners
 [
     "visibilitychange",
